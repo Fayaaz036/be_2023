01a0eaed003d17bdf2bf2ea554d4c17394620535
1190190 <96318680+1190190@users.noreply.github.com>
2023-02-16 14:55:18 +0100

    Express werkend met Node

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-    res.send('nodemon aangepast')
+    res.send('nodemon aangepast,')
 })
 app.listen(port, ()=> {
     console.log('Shaking it up on ' + port)
