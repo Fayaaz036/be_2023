f257bbcdda7a17e03adf2f7dd1212518e270d33d
1190190 <96318680+1190190@users.noreply.github.com>
2023-03-09 19:32:20 +0100

    eerste paar ejs templates werkend gekregen

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+
+
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
 const port = 3000
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home.ejs')
 })
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    res.render('info.ejs', { username, password }); // toont de data op showData.ejs
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
