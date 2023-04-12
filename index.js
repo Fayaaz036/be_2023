019df50b92a52abf6352136de80316ec1d8b9608
1190190 <fayaaz.nabie@hva.nl>
2023-04-12 12:59:18 +0200

    .

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 // Require external data/functions
 const express = require('express')
+const multer = require('multer')
 const expressLayouts = require('express-ejs-layouts')
 const dotenv = require('dotenv').config()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const compression = require('compression')
 //Album en user model met hashpassword in db
-const  Users = require('./models/models')
-const saltRounds = 5;
+const { Albums, Users } = require('./models/models')
+const saltRounds = 10
 
 // Defining express as app
 const app = express()
 
 // Compress all HTTP responses
+app.use(compression())
 
+// creating a session
+app.use(
+	session({
+		secret: process.env.SESSION_KEY,
+		resave: true,
+		saveUninitialized: true,
+		cookie: { maxAge: 600000 },
+	})
+)
 
 // check if user is authorized (logged in) to visit a page
+const authorizeUser = (req, res, next) => {
+	if (!req.session.user) {
+		res.status(401).render('401')
+	} else {
+		next()
+	}
+}
 
 // env variables
 const userName = process.env.USERNAME
@@ -23,7 +43,7 @@ const passWord = process.env.PASSWORD
 const port = process.env.PORT
 
 // Mongodb url
-const url = `mongodb+srv://${userName}:${passWord}@bloktech.6qhartg.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://${userName}:${passWord}@Database.ymup0ov.mongodb.net/?retryWrites=true&w=majority`
 
 // Making connection with Mongodb
 mongoose.set('strictQuery', false)
@@ -36,6 +56,21 @@ mongoose
 		console.log('error', e)
 	})
 
+// Defing the storage object for Multer
+const storage = multer.diskStorage({
+	// Giving a destination to the uploaded images
+	destination: (req, file, cb) => {
+		cb(null, 'public/images')
+	},
+	// Giving a file name to the uploaded images
+	// Added the current date to make sure their will be no duplicate images name
+	filename: (req, file, cb) => {
+		console.log('file', file)
+		cb(null, Date.now() + '-' + file.originalname)
+	},
+})
+
+const upload = multer({ storage: storage })
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs')
@@ -51,13 +86,74 @@ app.use(express.static(__dirname + '/public'))
 
 // All Get requests
 app.get('/', (req, res) => {
-	res.render('logIn', {
+	res.render('login', {
 		errorMessage: '',
 		errorClass: '',
 		emailInput: '',
 		passwordInput: '',
 	})
 })
+	.get('/results', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+		const fetchAlbums = await Albums.find({})
+		res.render('results', {
+			data: fetchAlbums,
+			user: favoriteAlbumTitles,
+			userinfo: currentUser,
+		})
+	})
+	.get('/preference', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		res.render('preference', { userinfo: currentUser })
+	})
+	.get('/results:id', authorizeUser, async (req, res) => {
+		const fetchOneAlbum = await Albums.find({ _id: req.params.id })
+		res.render('detailPageResults', { data: fetchOneAlbum })
+	})
+	.get('/favorites:id', authorizeUser, async (req, res) => {
+		const fetchOneAlbum = await Albums.find({ _id: req.params.id })
+		res.render('detailPageFavorites', { data: fetchOneAlbum })
+	})
+	.get('/all:id', authorizeUser, async (req, res) => {
+		const fetchOneAlbum = await Albums.find({ _id: req.params.id })
+		res.render('detailPageAll', { data: fetchOneAlbum })
+	})
+	.get('/favorites', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbums = currentUser[0].Like
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+		res.render('favorites', {
+			data: favoriteAlbums,
+			user: favoriteAlbumTitles,
+			userinfo: currentUser,
+		})
+	})
+	.get('/deleteModal:id', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const fetchAlbum = await Albums.find({ _id: req.params.id })
+		res.render('deleteModal', { data: fetchAlbum, userinfo: currentUser })
+	})
+	.get('/add', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		res.render('add', { userinfo: currentUser })
+	})
+	.get('/all', authorizeUser, async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+
+		const fetchAlbums = await Albums.find({}).sort({ _id: -1 })
+		res.render('all', { data: fetchAlbums, user: favoriteAlbumTitles, userinfo: currentUser })
+	})
+	.get('/update', authorizeUser, async (req, res) => {
+		const fetchOneUser = await Users.find({ _id: req.session.user.userID })
+		const currentUser = await Users.find({ _id: fetchOneUser[0]._id })
+		res.render('update', { data: currentUser, passError: 'false' })
+	})
+	.get('/succesUpdate', authorizeUser, (req, res) => {
+		res.render('succesUpdate')
+	})
+
 	.get('/register', async (req, res) => {
 		res.render('register', {
 			errorMessage: '',
@@ -65,8 +161,7 @@ app.get('/', (req, res) => {
 		})
 	})
 	.get('/registerSucces', async (req, res) => {
-		res.render(
-			'registerSucces')
+		res.render('registerSucces')
 	})
 	.get('*', (req, res) => {
 		res.status(404).render('404')
@@ -75,7 +170,7 @@ app.get('/', (req, res) => {
 const errorlogin = req => {
 	return {
 		errorMessage: 'Email or password incorrect',
-		errorClass: 'errorlogin',
+		errorClass: 'errorLogin',
 		emailInput: req.body.email,
 		passwordInput: req.body.password,
 	}
@@ -91,7 +186,9 @@ app.post('/home', async (req, res) => {
 		const cmp = await bcrypt.compare(req.body.password, dbpw)
 		// when password is identical with the one in the database, create a session with user ID
 		if (cmp) {
-			res.render('preference')
+			req.session.user = { userID: checkUser[0]['_id'] }
+			const currentUser = await Users.find({ _id: req.session.user.userID })
+			res.render('preference', { userinfo: currentUser })
 		} else {
 			// show error message when password is wrong
 			res.render('login', errorlogin(req))
@@ -102,6 +199,8 @@ app.post('/home', async (req, res) => {
 	}
 })
 	.post('/logout', (req, res) => {
+		// when user logs out destroy the session
+		req.session.destroy()
 		// display success message in log in page
 		res.render('login', {
 			errorMessage: 'You are logged out',
@@ -110,16 +209,157 @@ app.post('/home', async (req, res) => {
 			passwordInput: '',
 		})
 	})
-	.post('/register', async (req, res) => {
-		// const checkUser = await Users.find({ Email: req.body.email })
-		// const uname = checkUser['Username']
+	.post('/results', async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+
+		const fetchAlbums = await Albums.find({ Year: req.body.year, Genre: req.body.genre })
+		res.render('results', {
+			data: fetchAlbums,
+			user: favoriteAlbumTitles,
+			userinfo: currentUser,
+		})
+	})
+	.post('/favorites:id', async (req, res) => {
+		const currentUser = await Users.findOne({ _id: req.session.user.userID })
+		const currentAlbum = await Albums.findOne({ _id: req.params.id })
+		const albumTitle = currentUser.Like.map(item => item.Title)
+
+		if (albumTitle.includes(currentAlbum.Title)) {
+			const updateFavorite = await Users.findOneAndUpdate(
+				{ _id: currentUser.id },
+				{ $pull: { Like: currentAlbum } }
+			)
+		} else {
+			const updateFavorite = await Users.findOneAndUpdate(
+				{ _id: currentUser.id },
+				{ $push: { Like: currentAlbum } }
+			)
+		}
+	})
+	.post('/add', upload.single('File'), async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+
+		Albums.insertMany([
+			{
+				Title: req.body.Title,
+				Artist: req.body.Artist,
+				Genre: req.body.Genre,
+				Year: req.body.Year,
+				Like: false,
+				Description: req.body.Description,
+				Image: { data: req.file.filename, contentType: 'image/png' },
+				SpotifyLink: req.body.SpotifyLink,
+			},
+		]).then(() => console.log('user saved'))
+
+		res.render('succesAdd', { userinfo: currentUser })
+	})
+	.post('/delete:id', async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+
+		const deleteAlbum = await Albums.find({ _id: req.params.id }).remove()
+		const fetchAlbums = await Albums.find({}).sort({ _id: -1 })
+		res.render('all', { data: fetchAlbums, user: favoriteAlbumTitles, userinfo: currentUser })
+	})
+	.post('/all', async (req, res) => {
+		const currentUser = await Users.find({ _id: req.session.user.userID })
+		const favoriteAlbumTitles = currentUser[0].Like.map(item => item.Title)
+
+		const fetchAlbums = await Albums.find({
+			$or: [
+				{ Title: req.body.search },
+				{ Artist: req.body.search },
+				{ Year: req.body.search },
+				{ Genre: req.body.search },
+			],
+		})
+		res.render('all', { data: fetchAlbums, user: favoriteAlbumTitles, userinfo: currentUser })
+	})
+
+	.post('/update', upload.single('profilePicture'), async (req, res) => {
+		try {
+			//fetch user
+			const fetchOneUser = await Users.find({ _id: req.session.user.userID })
+			const currentUser = await Users.find({ _id: fetchOneUser[0]._id })
+
+			var newUsername = { $set: { Username: req.body.newUsername } }
+			var newEmail = { $set: { Email: req.body.newEmail } }
+
+			//current password equals current password in database in hash
+			const hashCheck = await bcrypt.compare(
+				req.body.currentPassword,
+				currentUser[0].Password
+			)
+			//hash new password
+			const hashedPwd = await bcrypt.hash(req.body.newPassword, saltRounds)
+			var newPassword = { $set: { Password: hashedPwd } }
+
+			//if profile picture is empty keep current profile picture
+			if (req.file == undefined) {
+				var newProfilePic = {
+					$set: {
+						Profilepic: {
+							data: currentUser[0].Profilepic.data,
+							contentType: 'image/png',
+						},
+					},
+				}
+				console.log('there is no file uploaded')
+			}
+
+			if (req.file != undefined) {
+				newProfilePic = {
+					$set: { Profilepic: { data: req.file.filename, contentType: 'image/png' } },
+				}
+				console.log('there is a file uploaded')
+			}
+
+			//if username is empty keep current username
+			if (req.body.newUsername == '') {
+				newUsername = currentUser[0].Username
+				console.log('there is no new username')
+			}
+
+			//if email is empty keep current email
+			if (req.body.newEmail == '') {
+				newEmail = currentUser[0].Email
+				console.log('there is no new email')
+			}
+
+			//if password not empty and current password is not the same as current password in database
+			if (
+				(req.body.currentPassword !== '' && hashCheck === false) ||
+				(req.body.currentPassword == '' && req.body.newPassword !== '')
+			) {
+				return res.render('update', { data: currentUser, passError: true })
+			} else if (hashCheck === true && req.body.newPassword == '') {
+				return res.render('update', { data: currentUser, passError: true })
+			}
+
+			//update user
+			await Users.findOneAndUpdate({ _id: currentUser[0]._id }, newProfilePic)
+			await Users.findOneAndUpdate({ _id: currentUser[0]._id }, newUsername)
+			await Users.findOneAndUpdate({ _id: currentUser[0]._id }, newEmail)
+			await Users.findOneAndUpdate({ _id: currentUser[0]._id }, newPassword)
+
+			res.redirect('/succesUpdate')
+		} catch (error) {
+			console.log(error)
+		}
+	})
+
+	.post('/register', upload.single('Profilepic'), async (req, res) => {
+		const checkUser = await Users.find({ Email: req.body.email })
+		const uname = checkUser['Username']
 		Users.findOne({ Email: req.body.email }, async (err, result) => {
 			if (err) throw err
 			if (result) {
 				// doe hier iets om te melden dat het e-mailadres al in gebruik is
 				res.render('register', {
 					errorMessage: 'Email allready exist',
-					errorClass: 'errorlogin',
+					errorClass: 'errorLogin',
 				})
 			} else {
 				// als de email niet in gebruik is, voor onderstaande commando uit
@@ -129,6 +369,7 @@ app.post('/home', async (req, res) => {
 						Username: req.body.username,
 						Password: hashedPwd,
 						Email: req.body.email,
+						Profilepic: { data: req.file.filename, contentType: 'image/png' },
 					},
 				]).then(() => console.log('user saved'))
 				res.redirect('registerSucces')
--- models/models.js
+++ models/models.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose')
 
+const albumSchema = new mongoose.Schema({
+	Title: String,
+	Artist: String,
+	Genre: String,
+	Year: String,
+	Image: {
+		data: String,
+		contentType: String,
+	},
+	Description: String,
+	Like: Boolean,
+	SpotifyLink: String,
+})
+
 const userSchema = new mongoose.Schema({
 	Email: String,
 	Like: Array,
 	Password: String,
+	Profilepic: {
+		data: String,
+		contentType: String,
+	},
 	Username: String,
 })
 
-const Users = mongoose.model('users', userSchema, 'playstationapp')
-module.exports =  Users;
+const Albums = mongoose.model('Albums', albumSchema, 'Albums')
+const Users = mongoose.model('Users', userSchema, 'Users')
+module.exports = { Users, Albums }
